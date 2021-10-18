f045f5d2564e8abc754feae24b106eaa8507ffc5
Ibnu Katmo At Tirthamy <ahmadjayadibya@gmail.com>
2021-10-18 13:10:55 +0700

    fix tugas 2

---
+++ Week02-2020070020/src/SelisihUmur.java
@@ -0,0 +1,9 @@
+public class SelisihUmur {
+    public static void main(String[] args) {
+        int ranu = 24;
+        int rita = 28;
+        int diff = Math.abs(ranu-rita);
+
+        System.out.println(diff);
+    }
+}
